a257796a71dbe781134d98cefb5ecbf4b6dd9745
Santiago Da Silva <sdasilva@outlook.es>
2020-11-01 18:25:39 -0300

    getUsuarios()

--- src/com/remote/UsuarioBo.java
+++ src/com/remote/UsuarioBo.java
@@ -1,8 +1,11 @@
 package com.remote;
 
+import java.util.List;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import com.entities.Usuario;
 import com.service.UsuarioBeanRemote;
 
 public class UsuarioBo {
@@ -10,12 +13,19 @@ public class UsuarioBo {
 	private String context;
 	private UsuarioBeanRemote usuarioBean;
 
-	public UsuarioBo() throws NamingException {
+	public UsuarioBo() {
 		super();
 		context = "ejb:/IAgro-Server/UsuarioBean!com.service.UsuarioBeanRemote";
-		usuarioBean = (UsuarioBeanRemote)InitialContext.doLookup(context);
+		try {
+			usuarioBean = (UsuarioBeanRemote)InitialContext.doLookup(context);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	
+	public List<Usuario> getUsuarios() {
+		return usuarioBean.readAll();
+	}
 
 }
