cc9c65e5e0362f31759fe6a908954c99def7ed58
Santiago Da Silva <sdasilva@outlook.es>
2020-11-01 15:03:52 -0300

    remote de auth con login y getter de usuario autenticado

---
+++ src/com/remote/AuthBo.java
@@ -0,0 +1,33 @@
+package com.remote;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.entities.Usuario;
+import com.service.AuthBeanRemote;
+
+public class AuthBo {
+	
+	private String context;
+	private AuthBeanRemote authBean;
+	
+	public AuthBo() {
+		super();
+		context = "ejb:/IAgro-Server/AuthBean!com.service.AuthBeanRemote?stateful";
+		try {
+			authBean = (AuthBeanRemote)InitialContext.doLookup(context);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void login(String username, String password) {
+		authBean.login(username, password);
+	}
+	
+	public Usuario getAuthUser() {
+		return authBean.getAuthUser();
+	}
+
+}
