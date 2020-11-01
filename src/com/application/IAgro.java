649b55731461172418f37978747d0e3425ae4b3c
Santiago Da Silva <sdasilva@outlook.es>
2020-11-01 15:05:37 -0300

    metodos para login y getter de usuario autenticado en el remote

--- src/com/application/IAgro.java
+++ src/com/application/IAgro.java
@@ -1,10 +1,13 @@
 package com.application;
 
+import com.entities.Usuario;
 import com.presentation.Login;
+import com.remote.AuthBo;
 
 public class IAgro {
 	
 	private Login login; // capa presentacion de login
+	private AuthBo auth; // capa de negocios de autenticacion de usuarios
 
 	public static void main(String[] args) {
 		IAgro iagro = new IAgro(); // Se crea una instancia
@@ -24,7 +27,24 @@ public class IAgro {
 	 * Metodo para arrancar IAgro con la capa de negocios inicial y mostrando login
 	 */
 	private void start() {
+		auth = new AuthBo();
 		login.start(); // muestro la ventana de login con al menos 1 usuario por defecto cargado en la BD
+	}
+	
+	//***********************************************
+	// Metodos para el manejo de la capa presentacion
+	//***********************************************
+	
+	//**********************************************
+	// Metodos para el manejo de la capa de negocios
+	//**********************************************
+	
+	public void login(String username, String password) {
+		auth.login(username, password);
+	}
+	
+	public Usuario getAuthUser() {
+		return auth.getAuthUser();
 	}
 
 }
