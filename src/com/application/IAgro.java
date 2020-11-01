686e19e1e7c705c1ad190feb6e3acc9baca13204
Santiago Da Silva <sdasilva@outlook.es>
2020-11-01 12:19:20 -0300

    start() y Constructor con login inyectando iagro

--- src/com/application/IAgro.java
+++ src/com/application/IAgro.java
@@ -1,10 +1,30 @@
 package com.application;
 
+import com.presentation.Login;
+
 public class IAgro {
+	
+	private Login login; // capa presentacion de login
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		IAgro iagro = new IAgro(); // Se crea una instancia
+		iagro.start();
+	}
+	
+	/**
+	 * 
+	 * Constructor de IAgro con la UI inicial de login sin mostrarla
+	 */
+	public IAgro() {
+		login = new Login(this); // Inyecto esta instancia a login
+	}
+	
+	/**
+	 * 
+	 * Metodo para arrancar IAgro con la capa de negocios inicial y mostrando login
+	 */
+	private void start() {
+		login.start(); // muestro la ventana de login con al menos 1 usuario por defecto cargado en la BD
 	}
 
 }
