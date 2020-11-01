5236ad0d801fedf0b2be4bda0b8b90eeffccd05b
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-01 12:17:25 -0300

    Creado Clase FuncionalidadBo

--- src/com/remote/FuncionalidadBo.java
+++ src/com/remote/FuncionalidadBo.java
@@ -1,5 +1,20 @@
 package com.remote;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.service.FuncionalidadBeanRemote;
+
 public class FuncionalidadBo {
+	
+	private String context;
+	
+	private FuncionalidadBeanRemote funcionalidadBean;
+	
+	public FuncionalidadBo() throws NamingException {
+		super();
+		context="";
+		funcionalidadBean = (FuncionalidadBeanRemote)InitialContext.doLookup(context);
+	}
 
 }
