6c7271ad770bfdb42478637c2dc0ce1d70064690
Santiago Da Silva <sdasilva@outlook.es>
2020-11-14 23:33:25 -0300

    elimino usos de rol y funcionalidad

--- src/com/presentation/MenuPrincipal.java
+++ src/com/presentation/MenuPrincipal.java
@@ -131,7 +131,7 @@ public class MenuPrincipal {
 		JButton btnCrearFuncionalidades = new JButton("Crear");
 		btnCrearFuncionalidades.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				iagro.show(CrearFuncionalidad.class);
+				
 			}
 		});
 		btnCrearFuncionalidades.setBounds(6, 11, 89, 23);
@@ -140,7 +140,7 @@ public class MenuPrincipal {
 		JButton btnListarFuncionalidades = new JButton("Listar");
 		btnListarFuncionalidades.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				iagro.show(ListarFuncionalidades.class);
+				
 			}
 		});
 		btnListarFuncionalidades.setBounds(6, 45, 89, 23);
@@ -165,7 +165,7 @@ public class MenuPrincipal {
 		JButton btnCrearRoles = new JButton("Crear");
 		btnCrearRoles.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				iagro.show(CrearRol.class);
+				
 			}
 		});
 		btnCrearRoles.setBounds(10, 11, 107, 23);
@@ -174,7 +174,7 @@ public class MenuPrincipal {
 		JButton btnListarRoles = new JButton("Listar");
 		btnListarRoles.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				iagro.show(ListarRoles.class);
+				
 			}
 		});
 		btnListarRoles.setBounds(10, 45, 107, 23);
