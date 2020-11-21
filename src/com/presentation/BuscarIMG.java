8a3b570aea8bbcb000a19ea689cca93585f15ae8
WildIndex <carlossuarez464@gmail.com>
2020-11-21 19:23:57 -0300

    creo la ventana del buscador de imagenes

---
+++ src/com/presentation/BuscarIMG.java
@@ -0,0 +1,52 @@
+package com.presentation;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JDesktopPane;
+import java.awt.BorderLayout;
+import javax.swing.JFileChooser;
+
+public class BuscarIMG {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BuscarIMG window = new BuscarIMG();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BuscarIMG() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 590, 437);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		frame.getContentPane().add(desktopPane, BorderLayout.CENTER);
+		
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setBounds(0, 0, 582, 399);
+		desktopPane.add(fileChooser);
+	}
+}
