6eb5930729e425139a149122cf1b3ff0ff97c086
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-28 00:48:05 -0300

    Login transformo en mayusculas para evitar el error por no coincidir el nickname de la base de datos con lo ingresado

--- src/com/presentation/Login.java
+++ src/com/presentation/Login.java
@@ -117,7 +117,7 @@ public class Login {
 		btnEntrar.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		btnEntrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String username = textFieldUser.getText();
+				String username = textFieldUser.getText().toUpperCase();
 				String password = passwordFieldPass.getText();
 				iagro.login(username, password); // seteo el usuario autenticado
 				if(iagro.getAuthUser() != null) { // si se encuentra un usuario
